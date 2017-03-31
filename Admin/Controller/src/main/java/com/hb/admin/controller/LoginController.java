22406d1ae87b5e52e0f12dc9362b61626d011029
47 <136698493@qq.com>
2017-03-31 23:02:21 +0800

    项目结构

---
+++ Admin/Controller/src/main/java/com/hb/admin/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.hb.admin.controller;
+
+/**
+ * Created by hp on 2017/3/31.
+ */
+public class LoginController {
+    public  static void main(String[] args){
+        System.out.print(0);
+    }
+
+}
